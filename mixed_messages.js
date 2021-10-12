8a59e302b0187a0fb814f1061d5243f985bb64b1
Daniel Campos <danielcampos@Daniels-UsabilityHub-MacBook-Pro.local>
2021-10-12 00:50:19 -0500

    First commit: setup the mixed messages directory, created a javascript file for the project, and used the command line to verify that node.js was set up and able to run the file.

---
+++ mixed_messages.js
@@ -0,0 +1,2 @@
+const message = "This is a test to see if Node.js is set up on this computer and can run this Javascript code. I'll be using this file to create the Mixed Messages Project for Codecademy.";
+console.log(message);
