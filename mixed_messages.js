8495347c00f90c43ec190a6d03036293bdb1f61b
Daniel Campos <danielcampos@Daniels-UsabilityHub-MacBook-Pro.local>
2021-10-24 17:38:17 -0500

    Worked with Tristan during today's code pairing session to help troubleshoot functionality and review some concepts. We changed a .forEach loop to a .map that takes the values of the object key's and puts them into a new array so that they may be loged in a sentence format.

--- mixed_messages.js
+++ mixed_messages.js
@@ -4,17 +4,51 @@ console.log(message);
 */
 
 const messageComponents = {
-    comp1: ['I'],
-    comp2: ['Am'],
-    comp3: ['Daniel']
+    comp1: ["January", "February", "March", "April", "May", "June", "July"],
+    comp2: ["I", "Am", "Daniel"],
+    comp3: ["This", "is", "Sparta!"]
 };  
 
+const values = Object.values(messageComponents);
+console.log(values);
 
+const messageBits = values.map(val => {
+    const random = Math.floor(Math.random() * val.length);
+    return val[random];
+});
+
+console.log(`Today is my: ${messageBits[0]}. Because of ${messageBits[1]}. And Tristan said ${messageBits[2]}`);
+
+/*
 for (let components in messageComponents) {
-    console.log(`${messageComponents[components]}`);
+    for (let i = 0; i < components.length - 2; i++) {
+        components = messageComponents.comp'\i'[Math.floor(Math.random() * messageComponents.comp[i].length)]
+        console.log(`${messageComponents[components]}`);
+    }
 };
+*/
+
+//const keys = Object.keys(messageComponents);
+//console.log(keys);
 
 
+
+/*
+for (let components in messageComponents) {
+    const values = Object.values(messageComponents);
+    const random = Math.floor(Math.random() * values.length);
+
+    console.log(random, values[random])
+};
+*/
+
+
+
+
+
+/*
 let composeMessage = obj => {
-
+    const random = Math.floor(Math.random() * messageComponents.length);
+    return console.log(random, messageComponents[random]);
 };
+*/
