b522d2c777472364f7dc18ecb314a3096373f806
Daniel Campos <danielcampos@Daniels-UsabilityHub-MacBook-Pro.local>
2021-10-24 12:25:45 -0500

    Second commit: created an Object literal that will store 3 separate arrays as the 'message components' that will comprise the final message. Tested a for..in loop to print the content inside of the arrays inside of the object literal. Added a function that will take in the object literal and compose the message by randomly iterating through each component in the object.

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,2 +1,20 @@
+/*
 const message = "This is a test to see if Node.js is set up on this computer and can run this Javascript code. I'll be using this file to create the Mixed Messages Project for Codecademy.";
 console.log(message);
+*/
+
+const messageComponents = {
+    comp1: ['I'],
+    comp2: ['Am'],
+    comp3: ['Daniel']
+};  
+
+
+for (let components in messageComponents) {
+    console.log(`${messageComponents[components]}`);
+};
+
+
+let composeMessage = obj => {
+
+};
